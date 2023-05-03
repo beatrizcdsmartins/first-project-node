157883ba4ccb7ad4b8df2ecf92c9860a33a27343
beatrizcdsmartins <beatrizcdsmartins@gmail.com>
2023-05-03 10:33:15 -0300

    hamburguer-react-commit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const uuid = require('uuid')
+const cors = require('cors')
 const port = 3002
 const app = express ()
 app.use(express.json())
 
-
-
+app.use(cors())
 
 const users = []
-
 
 const idvalidation = (request, response, next) =>{
 
@@ -23,17 +22,10 @@ const idvalidation = (request, response, next) =>{
 
     request.userIndex = index 
     request.userid=id
-
-   
-
     next()
 
-
-
 }
-
-
-
+ 
 app.get('/users', (request, response) =>{  // GET -> apresentation
 
     return response.json(users)
@@ -41,9 +33,9 @@ app.get('/users', (request, response) =>{  // GET -> apresentation
 })
 
 app.post('/users', (request, response) =>{ // POST -> add things
-    const {name, age } = request.body
+    const {name, order } = request.body
 
-    const user = {id:uuid.v4(), name, age}
+    const user = {id:uuid.v4(), order, name}
 
     users.push(user)
 
@@ -55,20 +47,16 @@ app.put('/users/:id', idvalidation, (request, response) =>{ // PUT ->  to update
 
 
     const index = request.userIndex
-    const {name, age} = request.body
+    const {name, order} = request.body
     const id = request.userid
 
-    const updateUser = {id, name, age}
-
-    
+    const updateUser = {id, order, name}
 
     users[index]= updateUser
-
 
     return response.json(updateUser)
 
 })
-
 
 app.delete('/users/:id', idvalidation, (request, response) =>{ // DELETE
 
@@ -77,14 +65,11 @@ app.delete('/users/:id', idvalidation, (request, response) =>{ // DELETE
     users.splice(index, 1)
 
 
-
     return response.status(204).json()
 
 })
 
- 
 // port
-
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}💕`)
 })
