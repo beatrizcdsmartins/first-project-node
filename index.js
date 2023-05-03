03079684df84f67d0533611da40a5a26c338b4b4
beatrizcdsmartins <beatrizcdsmartins@gmail.com>
2023-05-03 15:18:30 -0300

    change door

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const uuid = require('uuid')
 const cors = require('cors')
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 const app = express ()
 app.use(express.json())
 
